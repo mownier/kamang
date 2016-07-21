e079a4b6e37b4787553a3c29636d5f397c656814
Mounir Ybanez <rinuom91@gmail.com>
2016-07-21 14:02:46 +0800

    Added Goblin scene.

---
+++ source/characters/goblin/goblin.gd
@@ -0,0 +1,12 @@
+
+extends KinematicBody2D
+
+# 8 directions, 48 frames per direction 
+# (stance, hop, charge, attack, throw, block, hit, die, crit-die)
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
