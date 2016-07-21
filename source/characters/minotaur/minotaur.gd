59aff33479ce7ea717bec40c136b466556092193
Mounir Ybanez <rinuom91@gmail.com>
2016-07-21 14:01:12 +0800

    Modified Minotaur scene.

--- source/characters/minotaur/minotaur.gd
+++ source/characters/minotaur/minotaur.gd
@@ -1,23 +1,40 @@
+tool
 
 extends KinematicBody2D
+
+var do_stance = true
+var do_rush = false
+var do_attack = false
 
 onready var anim = get_node("sprite/anim")
 onready var g = get_node("/root/global")
 onready var action = g.Action.new()
 
 func _ready():
+	var pos = Vector2(568/2, 320/2) 
+	set_pos(pos)
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	pass
+	if do_stance:
+		stance(g.MOVE_EAST)
+	elif do_rush:
+		rush(g.MOVE_EAST)
+	elif do_attack:
+		if not anim.is_playing():
+			print("playing")
+			attack(g.MOVE_EAST)
 
 func stance(direction):
+	anim.set_speed(1/0.9)
 	process_action("stance", direction)
 
 func rush(direction):
+	anim.set_speed(1/0.8)
 	process_action("rush", direction)
 
 func attack(direction):
+	anim.set_speed(1/0.4)
 	process_action("attack", direction)
 
 func process_action(name, direction):
